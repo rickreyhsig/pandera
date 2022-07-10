22c0c21cfb74b1cf5cad396c16c44cb84d99f42b
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2022-07-09 20:43:53 -0400

    Update domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'https://garden-care.herokuapp.com',
     :authentication => :plain,
   }
 
